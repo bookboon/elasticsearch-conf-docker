069b59334a6acc407f9ccaf052f5210f012950ce
Lasse Krogh Mammen <lkm@bookboon.com>
2017-04-11 12:21:45 +0100

    Update for ES5

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
 
 PLUGIN_TXT=${PLUGIN_TXT:-/usr/share/elasticsearch/plugins.txt}
 
-mkdir /usr/share/elasticsearch/config/scripts
+mkdir -p /usr/share/elasticsearch/config/scripts
 
 while [ ! -f "/usr/share/elasticsearch/config/elasticsearch.yml" ]; do
     sleep 1
@@ -12,7 +12,9 @@ done
 
 if [ -f "$PLUGIN_TXT" ]; then
     for plugin in $(<"${PLUGIN_TXT}"); do
-        /usr/share/elasticsearch/bin/elasticsearch-plugin install $plugin
+        if [ ! -d "/usr/share/elasticsearch/plugins/$plugin" ]; then
+            /usr/share/elasticsearch/bin/elasticsearch-plugin install $plugin
+        fi
     done
 fi
 
